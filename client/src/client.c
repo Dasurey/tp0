0428ec8e4bab117766c77ac21a1f510751a26012
Dasurey <dario.asurey@gmail.com>
2024-09-07 00:10:37 +0000

    TP0 completo. Hice cambios en Utils.c de server porque desde la parte de pantalla hacia un \n demas.

--- client/src/client.c
+++ client/src/client.c
@@ -18,7 +18,7 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	log_info(logger, "Hola! Soy un log");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -27,21 +27,27 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
+	valor = config_get_string_value(config, "CLAVE");
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+
 	// Loggeamos el valor de config
 
+	log_info(logger, "VALOR leido de la config: %s", valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
 	leer_consola(logger);
 
 	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
-
+	
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
 	// Creamos una conexión hacia el servidor
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor, conexion);
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
@@ -50,18 +56,33 @@ int main(void)
 
 	/*---------------------------------------------------PARTE 5-------------------------------------------------------------*/
 	// Proximamente
+	printf("\nCLIENTE CERRADO!!!!!");
 }
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("tp0.log", "LOGGER_TP0", 1, LOG_LEVEL_INFO);
+
+	if (nuevo_logger == NULL) {
+    // ¡No se pudo crear el nuevo_logger!
+    // Terminemos el programa
+	perror("¡No se pudo crear el nuevo_logger! Terminemos el programa");
+	abort();
+	}
 
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config;
+	t_config* nuevo_config = config_create("/home/utnso/Desktop/TP0/tp0/client/cliente.config");
+
+	if (nuevo_config == NULL) {
+    // ¡No se pudo crear el nuevo_config!
+    // Terminemos el programa
+	perror("¡No se pudo crear el nuevo_config! Terminemos el programa");
+	abort();
+	}
 
 	return nuevo_config;
 }
@@ -71,25 +92,44 @@ void leer_consola(t_log* logger)
 	char* leido;
 
 	// La primera te la dejo de yapa
-	leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
 
-
 	// ¡No te olvides de liberar las lineas antes de regresar!
+	while (1) {
+        leido = readline("> ");
+        if (!strcmp(leido, "")) {
+            free(leido);
+            break;
+        }
+        log_info(logger, ">> %s", leido);
+        free(leido);
+    };
 
 }
 
 void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
-	char* leido;
-	t_paquete* paquete;
+	char* leido = NULL;
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
+	while (1) {
+        leido = readline("> ");
+        if (!strcmp(leido, "")) {
+			//ELIMINAR LINEAS
+            free(leido);
+            break;
+        }
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+        free(leido);
+    };
 
-
+	//ENVIAR PAQUETE
+	enviar_paquete(paquete, conexion);
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	eliminar_paquete(paquete);
 	
 }
 
@@ -97,4 +137,7 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
+	config_destroy(config);
+	liberar_conexion(conexion);
 }
